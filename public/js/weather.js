536dec3028f63e6b7a77738917f2c7fc3463eb40
Prathamesh-Shanbhag <prathameshshanbhag074@gmail.com>
2021-02-03 23:53:50 +0530

    Integrated Weather App with OpenWeatherMapAPI

--- public/js/weather.js
+++ public/js/weather.js
@@ -21,13 +21,22 @@ window.onload = function () {
   weatherBalloon(1275339);
 
   // "lon": 72.847939,
- // "lat": 19.01441
+  // "lat": 19.01441
 };
 function drawWeather(d) {
   var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
   var fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
+  var description = d.weather[0].description;
 
-  document.getElementById('description').innerHTML = d.weather[0].description;
+  document.getElementById('description').innerHTML = description;
   document.getElementById('temp').innerHTML = celcius + '&deg;';
   document.getElementById('location').innerHTML = d.name;
+
+  if (description.indexOf('rain') > 0) {
+    document.body.className = 'rainy';
+  } else if (description.indexOf('cloud') > 0) {
+    document.body.className = 'cloudy';
+  } else if (description.indexOf('sunny') > 0) {
+    document.body.className = 'sunny';
+  }
 }
